0ce187ca2b04ff977c63afc62ba429bc457f9216
Renan Carvalho <89706187+renancsilva94@users.noreply.github.com>
2026-05-09 01:58:53 -0300

    Update page.tsx
    "fix: usar dynamic import com ssr:false para App"

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 'use client'
 
-import App from '../src/App'
+import dynamic from 'next/dynamic'
+
+const App = dynamic(() => import('../src/App'), {
+  ssr: false,
+  loading: () => <div style={{minHeight: '100vh'}} />
+})
 
 export default function Home() {
   return <App />
