2dfc7c714b7fc85944edf683a51c85afab4068b1
Renan Carvalho <89706187+renancsilva94@users.noreply.github.com>
2026-05-09 10:19:55 -0300

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+  response.headers.set('x-pathname', request.nextUrl.pathname)
+  return response
+}
+ 
+export const config = {
+  matcher: ['/((?!_next|api|favicon.ico|images|.*\\..*).*)'],
+}
+ 
