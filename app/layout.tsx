6bb0ae7c86af4f194ae35955a6b5cf6dbada18c2
Renan Carvalho <89706187+renancsilva94@users.noreply.github.com>
2026-05-24 15:36:06 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -190,6 +190,7 @@ export async function generateMetadata(): Promise<Metadata> {
   }
 }
 
+
 export default function RootLayout({
   children,
 }: {
@@ -198,22 +199,31 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-S26Q4YLC2R"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-S26Q4YLC2R');
-            `,
-          }}
-        />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WDWKVBVP');`
+        }}/>
+        {/* GA4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S26Q4YLC2R"/>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S26Q4YLC2R');
+          `
+        }}/>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDWKVBVP"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}/>
         {children}
       </body>
     </html>
