f26dc36e594a005f98ee844a1453109502643eb0
Renan Carvalho <89706187+renancsilva94@users.noreply.github.com>
2026-06-24 15:45:53 -0300

    feat: add page instalacao-ar-condicionado-sapopemba

---
+++ app/instalacao-ar-condicionado-sapopemba/page.tsx
@@ -0,0 +1,40 @@
+import { Metadata } from 'next'
+import BairroView from '@/src/components/BairroView'
+
+export const metadata: Metadata = {
+  title: 'Instalação de Ar Condicionado em Sapopemba SP | New Clima',
+  description:
+    'Instalação e manutenção de ar condicionado em Sapopemba. Técnicos certificados, obra limpa e garantia por escrito. Visita técnica gratuita. (11) 96346-2516.',
+  alternates: {
+    canonical: 'https://www.newclimaar.com.br/instalacao-ar-condicionado-sapopemba',
+  },
+  openGraph: {
+    title: 'Instalação de Ar Condicionado em Sapopemba SP | New Clima',
+    description:
+      'Instalação e manutenção de ar condicionado em Sapopemba. Técnicos certificados, obra limpa e garantia por escrito. Visita técnica gratuita. (11) 96346-2516.',
+    url: 'https://www.newclimaar.com.br/instalacao-ar-condicionado-sapopemba',
+    type: 'website',
+    images: [{ url: 'https://www.newclimaar.com.br/images/logo-new-clima.png' }],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+}
+
+export default function Page() {
+  return (
+    <BairroView
+      bairro="Sapopemba"
+      slug="sapopemba"
+      zona="Zona Leste"
+      vizinhos={['Vila Prudente', 'São Mateus', 'Itaquera', 'Ermelino Matarazzo', 'Água Rasa']}
+    />
+  )
+}
